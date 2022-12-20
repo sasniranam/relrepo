b8fd43a14a1595720c8e73890e937865df991c4b
sasniranam <sasniranam@hotmail.com>
2022-12-20 11:13:05 +0530

    C pgms added

---
+++ currenttime.c
@@ -0,0 +1,12 @@
+#include <time.h>
+#include <stdio.h>
+ 
+int main()
+{
+        char time_str[100]; 
+        time_t current_time = time(NULL);
+        struct tm *tm = localtime(&current_time);
+        strftime(time_str, sizeof(time_str), "%c", tm);
+        printf("Current Date and Time:\n%s\n",time_str);
+        return 0;
+}
