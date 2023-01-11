48ccb0842c4b1a1482cd13c32fac49c5b38e980d
sasniranam <40558309+sasniranam@users.noreply.github.com>
2023-01-11 11:54:08 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 # Python program to print numbers from 1 to 100
 
-print('Numbers from 1 to 10000:')
-for n in range(1, 10001):
+print('Numbers from 1 to 1000:')
+for n in range(1, 1001):
     print(n, end=' ')
